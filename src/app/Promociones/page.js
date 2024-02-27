7a7bea1d9cdc93a59cb51e61677b80b10ef2081f
AngelLuna17-code <luna17ahle@gmail.com>
2024-02-27 14:40:24 -0700

    .

--- src/app/Promociones/page.js
+++ src/app/Promociones/page.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Promociones2 from '../../../components/promociones2';
 import Paquetes from '../../../components/Paquetes';
 
 const Promociones = () => {
     return (
         <main>
             <section>
-                <Promociones2 />
                 <Paquetes />
             </section>
         </main>
