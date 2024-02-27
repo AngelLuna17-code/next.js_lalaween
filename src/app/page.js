5152cfcf7794a218e98c048c66a25c334079af86
AngelLuna17-code <luna17ahle@gmail.com>
2024-02-27 14:16:13 -0700

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import Stars from "../../components/Stars";
 import Hero from "../../components/Hero"
-import ServiceCards from "../../components/ServiceCards";
-import SliderComponent from "../../components/SliderComponent";
+
 import Mapa from "../../components/Mapa";
 import Contacto from "../../components/Contacto";
 
@@ -15,12 +14,7 @@ export default function Home() {
         {/* Hero */}
         <Hero />
         {/* Hero */}
-        {/* ServiceCards */}
-        <ServiceCards />
-        {/* ServiceCards */}
-        {/* Portafolio */}
-        <SliderComponent />
-        {/* Portafolio */}
+
         {/* Mapa */}
         <Mapa />
         {/* Mapa */}
