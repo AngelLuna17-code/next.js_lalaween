3ea79f725d00c21bdef1df08636e9381ba64e23d
AngelLuna17-code <luna17ahle@gmail.com>
2024-02-28 14:26:14 -0700

    TRABAJANDO VISTA MODAL

--- components/ServiceCards.jsx
+++ components/ServiceCards.jsx
@@ -78,6 +78,7 @@ const ServiceCards = () => {
     return (
         <section>
             <div className='container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-5'>
+                {/* Cards */}
                 {list.map((item, index) => (
                     <div className="rounded-[15px]" key={index} style={{ backgroundColor: item.bgColor }}>
                         <div className='text-center p-5'>
@@ -104,74 +105,83 @@ const ServiceCards = () => {
                                 {item.text}
                             </p>
                         </div>
-
-                        {/* Botón para abrir el modal */}
-                        <div>
-                            <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                onClick={() => handleOpenModal(index)}>
-                                Abrir modal
-                            </button>
-                        </div>
-                        {/* Botón para abrir el modal */}
-                        {/* Modal */}
-                        {openModalIndex === index && (
-                            <div className="modal-overlay z-10 fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-                                <div className="modal bg-black p-8 rounded-[15px] min-w-[900px] w-[1300px]" style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
-                                    {/* header del modal */}
-                                    <div>
-                                        <div>
-                                            <p className='title text-2xl'>
-                                                {item.title}
-                                            </p>
-                                        </div>
-                                    </div>
-                                    {/* header del modal */}
-                                    {/* Botón para cerrar el modal */}
-                                    <button
-                                        className="close-btn absolute top-0 right-0 text-white bg-transparent border-none cursor-pointer"
-                                        onClick={handleCloseModal}>
-                                        X
-                                    </button>
-                                    {/* Botón para cerrar el modal */}
-                                    {/* Contenido del modal */}
-                                    <div>
-                                        <Slider {...settings}>
-                                            {item.images.map((image, index) => (
-                                                <div key={index}>
-                                                    <Image
-                                                        src={image}
-                                                        height={100}
-                                                        width={100}
-                                                        alt='Servicios'
-                                                    />
-                                                </div>
-                                            ))}
-                                        </Slider>
-                                    </div>
-                                    {/* Contenido del modal */}
-                                    {/* footer del modal */}
-                                    <div>
-                                        {/* Botón para cerrar el modal */}
-                                        <div>
-                                            <div className='mt-8'>
-                                                <button
-                                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                                    onClick={handleCloseModal}>
-                                                    Cerrar
-                                                </button>
-                                            </div>
-                                        </div>
-                                        {/* Botón para cerrar el modal */}
-                                    </div>
-                                    {/* footer del modal */}
-                                </div>
+                        {/* abrir modal */}
+                        <div className='relative cursor-pointer' onClick={() => handleOpenModal(index)}>
+                            <Image
+                                className='rounded-b-[15px]'
+                                height={0}
+                                width={500}
+                                src={`/cards/card-${index + 1}.webp`}
+                                alt="servicios"
+                                loading="lazy"
+                            />
+                            <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-hand-index-thumb-fill" viewBox="0 0 16 16">
+                                    <path d="M8.5 1.75v2.716l.047-.002c.312-.012.742-.016 1.051.046.28.056.543.18.738.288.273.152.456.385.56.642l.132-.012c.312-.024.794-.038 1.158.108.37.148.689.487.88.716q.113.137.195.248h.582a2 2 0 0 1 1.99 2.199l-.272 2.715a3.5 3.5 0 0 1-.444 1.389l-1.395 2.441A1.5 1.5 0 0 1 12.42 16H6.118a1.5 1.5 0 0 1-1.342-.83l-1.215-2.43L1.07 8.589a1.517 1.517 0 0 1 2.373-1.852L5 8.293V1.75a1.75 1.75 0 0 1 3.5 0" />
+                                </svg>
                             </div>
-                        )}
-                        {/* Modal */}
+                        </div>
+                        {/* abrir modal */}
                     </div>
                 ))}
+                {/* Cards */}
             </div>
+
+            {/* Modal */}
+            {openModalIndex !== null && (
+                <div className="modal-overlay z-10 fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+                    <div className="modal bg-black p-8 rounded-[15px] min-w-[900px] w-[1300px]" style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
+                        {/* header del modal */}
+                        <div>
+                            <div>
+                                <p className='title text-2xl'>
+                                    {list[openModalIndex].title}
+                                </p>
+                            </div>
+                        </div>
+                        {/* header del modal */}
+                        {/* Botón para cerrar el modal */}
+                        <button
+                            className="close-btn absolute top-0 right-0 text-white bg-transparent border-none cursor-pointer"
+                            onClick={handleCloseModal}>
+                            X
+                        </button>
+                        {/* Botón para cerrar el modal */}
+                        {/* Contenido del modal */}
+                        <div>
+                            <Slider className='W-[70%] MX-AUTO' {...settings}>
+                                {list[openModalIndex].images.map((image, index) => (
+                                    <div key={index}>
+                                        <img
+                                            className='w-auto h-[400px] object-contain mx-auto'
+                                            src={image}
+                                            alt={`slider-${index}`}
+                                            loading="lazy"
+                                        />
+                                    </div>
+                                ))}
+                            </Slider>
+                        </div>
+                        {/* Contenido del modal */}
+                        {/* footer del modal */}
+                        <div>
+                            {/* Botón para cerrar el modal */}
+                            <div>
+                                <div className='mt-8'>
+                                    <button
+                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                        onClick={handleCloseModal}>
+                                        Cerrar
+                                    </button>
+                                </div>
+                            </div>
+                            {/* Botón para cerrar el modal */}
+                        </div>
+                        {/* footer del modal */}
+                    </div>
+                </div>
+            )}
+            {/* Modal */}
         </section>
     );
 }
