8f797e93c7e2e1461c0a1b1d35b1a58ebf474021
AngelLuna17-code <luna17ahle@gmail.com>
2024-02-29 14:58:58 -0700

    estilando about

--- components/About.jsx
+++ components/About.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const About = () => {
     return (
         <section>
-            <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2'>
+            <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 py-[50%] lg:py-[100px]'>
                 <div className='flex items-center justify-center'>
                     <div className='text-white text-center'>
                         <h2 className='text-white text-center text-[25px] sm:text-[32px] md:text-[36px] lg:text-[40px] xl-[44px] 2xl:text-[48px] title-container '>
@@ -13,13 +13,12 @@ const About = () => {
                         <h2 className='text-white text-center text-[25px] sm:text-[32px] md:text-[36px] lg:text-[40px] xl-[44px] 2xl:text-[48px] title-container title'>
                             historia
                         </h2>
-                        <p className='w-[80%] mt-3 mx-auto'>
-                            Tal vez te has preguntado ¿por qué el logo de una agencia de publicidad es un gato y una luna?... pues detrás de esto hay una historia✨
-                            Él es Rony, mascota de nuestra fundadora, un gatito muy simpático, cariñoso y juguetón💖 Por él, este proyecto nació; la líder anhelaba una vida plena y alegre, trabajando en casa en compañía de su leal gato🐱
-                            Tristemente a Rony le toco partir antes de iniciar el plan🥹, pero nunca dejo de ser la fuente de inspiración de Lala (nuestra querida fundadora)🌟
-                            Ahora Rony está viéndonos desde la luna, rodeado de cometas a los que las personas puedan pedirle sus deseos💫
-                            Quizá Rony no esté en la oficina, pero dio pie a que sus sucesores, perros y gatos, lo estén😸
-                            🫶Es por eso que este día de la mascota queremos dedicárselo a Rony y a todos los fieles animalitos que acompañan e invaden corazones y familias🫶                        </p>
+                        <p className='w-[80%] mt-3 mx-auto text-center'>
+                            Nuestro logo, un gato y una luna, tiene una historia especial. Rony, la mascota de nuestra fundadora,
+                            fue la inspiración. Aunque ya no está físicamente, sigue siendo nuestra fuente de inspiración.
+                            Ahora imaginamos a Rony en la luna, donde las personas pueden pedir deseos. En el día de la mascota,
+                            honramos a Rony y a todos los animales que alegran nuestras vidas.
+                        </p>
                     </div>
                 </div>
                 <div className='flex items-center justify-center'>
