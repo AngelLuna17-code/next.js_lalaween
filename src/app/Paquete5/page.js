3a61cedc6c1bb193fbceb19dee814489b4b71977
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-27 10:19:42 -0700

    agregada paquete5

---
+++ src/app/Paquete5/page.js
@@ -0,0 +1,191 @@
+import SmallStars from '../../../components/SmallStars';
+
+const Paquete5 = () => {
+
+    return (
+        <section>
+            <SmallStars />
+            <div className='container mx-auto py-[100px] px-[10px]'>
+                <div className='p-5 rounded-lg' style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
+                    <div className='text-white'>
+                        <div>
+                            <h3 className='text-[#00a09b] font-black text-[23px] typing'>
+                                Pa' quete des a conocer
+                            </h3>
+                            <p>
+                                Inicia tu presencia en redes sociales y crece tu negocio
+                            </p>
+                        </div>
+                        <div className='mt-5'>
+                            <p className='text-[#00a09b] typing'>Proposito</p>
+                            <p>
+                                Diseñar, generar y ejecutar las bases para comenzar a promocionar tus servicios y/o productos en el
+                                mercado, estudiando y planteando estrategias según el análisis obtenido en redes sociales, dando a
+                                conocer y dejando al alcance de todos la comunicación directa y fluida con tu negocio en el mundo
+                                digital.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='mt-5'>
+                        <p className='text-[#00a09b] typing'>
+                            ¿Que implica?
+                        </p>
+                        <div>
+                            <ul className='typing'>
+                                <li>
+                                    Asesoramiento en la personalización de las redes sociales según su marca
+                                </li>
+                                <li>
+                                    Acompañamiento en el proceso de implementación en las campañas
+                                    publicitarias en redes sociales.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='mt-6'>
+                        <p className='text-[#00a09b] typing'>
+                            ¿Qué servicios incluye?
+                        </p>
+                    </div>
+                    {/* Pa’ que le empieces */}
+                    <div className='mt-5'>
+                        <div>
+                            <p className='text-[#00a09b] typing'>
+                                Pa’ que le empieces
+                            </p>
+                        </div>
+                        <div className='grid grid-cols-1 lg:grid-cols-2'>
+                            <div>
+                                <ul className='typing'>
+                                    <li>
+                                        Creación de cuentas para la marca en redes sociales (Facebook, Instagram y/o Tiktok)
+                                    </li>
+                                    <li>
+                                        Diseño de plantillas para redes sociales (Portada, Perfil y Publicaciones).
+                                    </li>
+                                    <li>
+                                        12 Publicaciones en imagen
+                                    </li>
+                                    <li>
+                                        8 Historias (imagen y video)
+                                    </li>
+                                    <li>
+                                        1 Social video para reel o tiktok.
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className='flex items-center justify-center'>
+                                <div className='text-center text-white typing'>
+                                    <p className='line-through'>COSTO REAL</p>
+                                    <p className='line-through'>$6,250.00 + IVA * Mes</p>
+                                    <p className='text-[#00a09b]'>COSTO PROMOCIONAL</p>
+                                    <p>$3,125.00 + IVA * Mes</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Pa’ que le empieces */}
+                    {/* Pa’ que te quieran */}
+                    <div className='mt-8'>
+                        <div>
+                            <p className='text-[#00a09b] typing'>
+                                Pa’ que te quieran
+                            </p>
+                        </div>
+                        <div className='grid grid-cols-1 lg:grid-cols-2'>
+                            <div>
+                                <ul className='typing'>
+                                    <li>
+                                        Creación de cuentas para la marca en redes sociales (Facebook, Instagram y/o Tiktok).
+                                    </li>
+                                    <li>
+                                        Diseño de plantillas para redes sociales(Portada, Perfil y Publicaciones)
+                                    </li>
+                                    <li>
+                                        20 Publicaciones en imagen.
+                                    </li>
+                                    <li>
+                                        2 Publicaciones en imagen tipo slide (Máximo 10 imagenes)
+                                    </li>
+                                    <li>
+                                        4 Historias (imagen y video).
+                                    </li>
+                                    <li>
+                                        2 Social video para reel o tiktok.
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className='flex items-center justify-center'>
+                                <div className='text-center text-white typing'>
+                                    <p className='line-through'>COSTO REAL</p>
+                                    <p className='line-through'>$8,400.00 + IVA * Mes</p>
+                                    <p className='text-[#00a09b]'>COSTO PROMOCIONAL</p>
+                                    <p>$4,200.00 + IVA * Mes</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Pa’ que te quieran */}
+                    {/* Pa’ que sueñen */}
+                    <div className='mt-8'>
+                        <div>
+                            <p className='text-[#00a09b] typing'>
+                                Pa’ que sueñen
+                            </p>
+                        </div>
+                        <div className='grid grid-cols-1 lg:grid-cols-2'>
+                            <div>
+                                <ul className='typing'>
+                                    <li>
+                                        Creación de cuentas para la marca en redes sociales (Facebook, Instagram y/o Tiktok).
+                                    </li>
+                                    <li>
+                                        Diseño de plantillas para redes sociales (Portada, Perfil y Publicaciones)
+                                    </li>
+                                    <li>
+                                        28 Publicaciones en imagen
+                                    </li>
+                                    <li>
+                                        28 Historias (imagen y video)
+                                    </li>
+                                    <li>
+                                        4 Social video para reel o tiktok
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className='flex items-center justify-center'>
+                                <div className='text-center text-white typing'>
+                                    <p className='line-through'>COSTO REAL</p>
+                                    <p className='line-through'>$16,400 + IVA * Mes</p>
+                                    <p className='text-[#00a09b]'>COSTO PROMOCIONAL</p>
+                                    <p>$8,200.00 + IVA * Mes</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mt-2'>
+                            <p className='text-[#00a09b] typing'>
+                                ¿Qué beneficios incluyen?
+                            </p>
+                            <div>
+                                <ul className='typing'>
+                                    <li>
+                                        Formato optimizado para Facebook e Instagram
+                                    </li>
+                                    <li>
+                                        Uso de imágenes de stock y/o proporcionadas por el cliente
+                                    </li>
+                                    <li>
+                                        Publicación del contenido trabajado para redes y administración de redes sociales durante un mes.
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Pa’ que sueñen */}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Paquete5;
