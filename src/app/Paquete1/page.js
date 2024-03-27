40a1297b512fd4acc573ae83807c2e60ca0df08a
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-27 13:55:55 -0700

    corrigiendo errores

--- src/app/Paquete1/page.js
+++ src/app/Paquete1/page.js
@@ -10,7 +10,7 @@ const Paquete1 = () => {
                     <div className='text-white'>
                         <div>
                             <h3 className='text-[#00a09b] font-black text-[23px] typing'>
-                                Pa&apos; que le emprendas
+                                Pa &apos; que le emprendas
                             </h3>
                             <p>En sus marcas, listos... ¡¡Fuera!!</p>
                         </div>
