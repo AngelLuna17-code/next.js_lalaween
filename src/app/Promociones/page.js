bec3f9cfd1597f3dcfadc3c35cdb315a17be4372
AngelLuna17-code <luna17ahle@gmail.com>
2024-04-01 14:51:15 -0700

    agregado hero

--- src/app/Promociones/page.js
+++ src/app/Promociones/page.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Paquetes from '../../../components/Paquetes';
 
 const Promociones = () => {
-    
+
     return (
         <main>
             <section>
+                <div className='paq-hero hero-bg py-[150px]'>
+                    <h1>
+                        Descubre
+                    </h1>
+                </div>
                 <Paquetes />
             </section>
         </main>
