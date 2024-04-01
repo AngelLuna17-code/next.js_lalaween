03988507422cbb6048f5f1d7d6e8b96a77e2ad98
AngelLuna17-code <luna17ahle@gmail.com>
2024-04-01 11:07:14 -0700

    agregado button para el flip

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,79 +4,49 @@ import Link from 'next/link';
 
 const Card = () => {
   const [cards, setCards] = useState([
-    /* Pa' que le emprendas */
     {
       id: 1,
-      /* Front Card */
       title: "Pa' que le emprendas",
       description: "En sus marcas, listos... ¡¡Fuera!!",
       oldPrice: "$8,100.00 + IVA",
       newPrice: "$3,500.00 + IVA",
-      /* Front Card */
-      /* Back Card */
       proposito: "Diseñar para comunicar de manera efectiva la personalidad, esencia e identidad de tu marca. Para generar un lanzamiento positivo desde la creación de su logotipo y a través de campañas publicitarias en redes sociales e impresos con diseños óptimos y atractivos para el nicho de mercado al que su negocio desee llegar, sin perder su autenticidad.",
       buttonLink: "/Paquete1"
-      /* Back Card */
     },
-    /* Pa' que le emprendas */
-    /* Pa' queten bien informados */
     {
       id: 2,
-      /* Front Card */
       title: "Pa' queten bien informados",
       description: "Ante una incognita se la solución",
       oldPrice: "$5,800.00 + IVA * Mes",
       newPrice: "3,780.00 + IVA * Mes",
-      /* Front Card */
-      /* Back Card */
       proposito: "Diseñar y ejecutar de manera efectiva campañas publicitarias e informativas sin que la marca pierda su esencia y autenticidad, a través de un lanzamiento positivo en redes sociales con diseños óptimos y atractivos para el nicho de mercado al que su negocio desee llegar",
       buttonLink: "/Paquete2"
-      /* Back Card */
     },
-    /* Pa' queten bien informados */
-    /* Pa' quete encuentren siempre */
     {
       id: 3,
-      /* Front Card */
       title: "Pa' quete encuentren siempre",
       description: "Ante la duda una solución",
       oldPrice: "$26,950.00 + IVA * Mes",
       newPrice: "$15,950.00 + IVA * Mes",
-      /* Front Card */
-      /* Back Card */
       proposito: "Mejorar la experiencia de sus usuarios y lograr un mayor alcance, con un máximo nivel de exposición posible para cumplir satisfactoriamente las necesidades de su nicho; conservando la autenticidad y personalidad de su marca, a través de un lanzamiento activo en redes sociales e implementación de su página web.",
       buttonLink: "/Paquete3"
-      /* Back Card */
     },
-    /* Pa' quete encuentren siempre */
-    /* Pa' que economices */
     {
       id: 4,
-      /* Front Card */
       title: "Pa' que economices",
       description: "Inicia tu presencia en redes sociales y crece tu negocio",
       oldPrice: "$5,250.00 + IVA * Mes",
       newPrice: "$2,625.00 + IVA * Mes",
-      /* Front Card */
-      /* Back Card */
       proposito: "Porque sabemos que vas empezando, pero el diseño no es lo tuyo o no tienes tiempo de crear contenido, con esto buscamos crear diseños atractivos y funcionales que le den el toque profesional a las redes sociales de tu negocio",
       buttonLink: "/Paquete4"
-      /* Back Card */
     },
-    /* Pa' que economices */
-    /* Pa' quete des a conocer */
     {
       id: 5,
-      /* Front Card */
-      title: "Pa' que economices",
+      title: "Pa' quete des a conocer",
       description: "Inicia tu presencia en redes sociales y crece tu negocio",
-      /* Front Card */
-      /* Back Card */
       proposito: "Diseñar, generar y ejecutar las bases para comenzar a promocionar tus servicios y/o productos en el mercado, estudiando y planteando estrategias según el análisis obtenido en redes sociales, dando a conocer y dejando al alcance de todos la comunicación directa y fluida con tu negocio en el mundo digita",
       buttonLink: "/Paquete5"
-      /* Back Card */
     },
-    /* Pa' quete des a conocer */
   ]);
 
   const [isFlipped, setIsFlipped] = useState([]);
@@ -87,13 +57,18 @@ const Card = () => {
     setIsFlipped(newFlippedState);
   };
 
+  const handleButtonClick = (index) => {
+    const newFlippedState = [...isFlipped];
+    newFlippedState[index] = !newFlippedState[index];
+    setIsFlipped(newFlippedState);
+  };
+
   return (
     <section className='py-[50px] px-[15px]'>
       <div className='container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-5'>
         {cards.map((card, index) => (
-          <div key={card.id} className={`card mx-auto cursor-pointer ${isFlipped[index] ? 'flipped' : ''}`} onClick={() => handleCardClick(index)}>
+          <div key={card.id} className={`card mx-auto ${isFlipped[index] ? 'flipped' : ''}`}>
             <div className="card-inner">
-              {/* Front */}
               <div className="card-front rounded-[15px] flex items-center justify-center" style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
                 <div>
                   <div className='text-center'>
@@ -114,10 +89,13 @@ const Card = () => {
                       </div>
                     )}
                   </div>
+                  <div className='flex justify-center'>
+                    <button className="flip-button mt-5 px-[15px] py-[10px] bg-[#006d6a] hover:bg-[#005351] font-bold rounded-[10px]" onClick={() => handleButtonClick(index)}>
+                      Más información
+                    </button>
+                  </div>
                 </div>
               </div>
-              {/* Front */}
-              {/* Back */}
               <div className="card-back rounded-[15px] flex items-center justify-center p-3 lg:p-5" style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
                 <div>
                   <div className='overflow-'>
@@ -131,13 +109,19 @@ const Card = () => {
                   <div className='flex justify-center mt-5'>
                     <Link href={card.buttonLink}>
                       <button className='px-[15px] py-[10px] bg-[#006d6a] hover:bg-[#005351] font-bold rounded-[10px]'>
-                        Más información
+                        Ver paquete
                       </button>
                     </Link>
                   </div>
+                  <div className='text-right'>
+                    <button className="flip-button mt-5" onClick={() => handleButtonClick(index)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                      </svg>
+                    </button>
+                  </div>
                 </div>
               </div>
-              {/* Back */}
             </div>
           </div>
         ))}
