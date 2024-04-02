05612d5d35c9762b7413daa5899b1595d0d6e167
AngelLuna17-code <luna17ahle@gmail.com>
2024-04-02 14:49:31 -0700

    corregido padding

--- components/About.jsx
+++ components/About.jsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 const About = () => {
     return (
-        <section>
-            <div id="about" className='container mx-auto grid grid-cols-1 lg:grid-cols-2 py-[50px]'>
+        <section className='py-[50px] px-[15px]'>
+            <div id="about" className='container mx-auto grid grid-cols-1 lg:grid-cols-2'>
                 <div className='flex items-center justify-center'>
                     <div className='text-white text-center'>
                         <h2 className='text-white text-center text-[25px] sm:text-[32px] md:text-[36px] lg:text-[40px] xl-[44px] 2xl:text-[48px] title-container '>
