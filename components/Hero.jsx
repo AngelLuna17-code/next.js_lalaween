12bb4f38d37d23f18ae7c44eefdb325c1975a861
AngelLuna17-code <luna17ahle@gmail.com>
2024-04-03 12:11:16 -0700

    espaciado

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,31 +6,34 @@ import Link from 'next/link';
 const Hero = () => {
 
     return (
-        <div className='container mx-auto px-[15px] sm:px-0 pt-[125px] pb-8 lg:pt-32 lg:pb-10'>
-            <div className='grid grid-cols-1 lg:grid-cols-2'>
-                <div className='flex items-center justify-center'>
-                    <Image
-                        src="/logo.webp"
-                        alt="logo"
-                        width={500}
-                        height={350}
-                        
-                    />
-                </div>
-                <div className='text-white text-center flex items-center justify-center overflow-hidden'>
-                    <div>
-                        <h1 className='font-thin'>Impulsamos</h1>
-                        <AnimatedText />
-                        <h1>conservando tu esencia</h1>
-                        <Link href="/Promociones">
-                            <button className='w-full lg:w-3/4 bg-[#066a69] hover:bg-[#01a09a] px-12 py-3 font-black rounded-lg mt-3'>
-                                Nuestros Paquetes
-                            </button>
-                        </Link>
+        <section className='px-[15px] pt-[125px] pb-8 lg:pt-32 lg:pb-10]'>
+            <div className='container mx-auto'>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+                    <div className='flex items-center justify-center'>
+                        <Image
+                            src="/logo.webp"
+                            alt="logo"
+                            width={500}
+                            height={350}
+
+                        />
+                    </div>
+                    <div className='text-white text-center flex items-center justify-center overflow-hidden'>
+                        <div>
+                            <h1 className='font-thin'>Impulsamos</h1>
+                            <AnimatedText />
+                            <h1>conservando tu esencia</h1>
+                            <Link href="/Promociones">
+                                <button className='w-full lg:w-3/4 bg-[#066a69] hover:bg-[#01a09a] px-12 py-3 font-black rounded-lg mt-3'>
+                                    Nuestros Paquetes
+                                </button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+
     );
 }
 
