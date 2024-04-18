4473e9ba431831a7579efbd0b4b3157bfc7baa59
AngelLuna17-code <luna17ahle@gmail.com>
2024-04-18 14:24:52 -0700

    trabajando animacion

--- components/Demo.jsx
+++ components/Demo.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
+import Image from 'next/image';
 
 function Demo() {
     return (
-        <div className="flip-box">
-            <div className="flip-box-inner">
-                <div className="flip-box-front">
-                    <img
-                        src="img_paris.jpg"
-                        alt="Paris"
-                    />
-                </div>
-                <div className="flip-box-back">
-
+        <section className='py-[50px] px-[15px]'>
+            <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2'>
+                <div className="flip-box">
+                    <div className="flip-box-inner">
+                        <div className="flip-box-front">
+                            <div>
+                                <Image
+                                    src="/portafolio/branding-1/item-1.webp"
+                                    alt="item"
+                                    width={300}
+                                    height={200}
+                                    loading='lazy'
+                                />
+                            </div>
+                        </div>
+                        <div className="flip-box-back">
+                            <div>
+                                <Image
+                                    src="/portafolio/branding-1/item-2.webp"
+                                    alt="item"
+                                    width={300}
+                                    height={200}
+                                    loading='lazy'
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
 
