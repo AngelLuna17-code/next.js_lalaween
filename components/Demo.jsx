dcf0c249c96bc4e32324ebacd4b9784db217dfd4
AngelLuna17-code <luna17ahle@gmail.com>
2024-04-18 15:00:52 -0700

    trabajando card

--- components/Demo.jsx
+++ components/Demo.jsx
@@ -3,36 +3,28 @@ import Image from 'next/image';
 
 function Demo() {
     return (
-        <section className='py-[50px] px-[15px]'>
-            <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2'>
-                <div className="flip-box">
-                    <div className="flip-box-inner">
-                        <div className="flip-box-front">
-                            <div>
-                                <Image
-                                    src="/portafolio/branding-1/item-1.webp"
-                                    alt="item"
-                                    width={300}
-                                    height={200}
-                                    loading='lazy'
-                                />
-                            </div>
-                        </div>
-                        <div className="flip-box-back">
-                            <div>
-                                <Image
-                                    src="/portafolio/branding-1/item-2.webp"
-                                    alt="item"
-                                    width={300}
-                                    height={200}
-                                    loading='lazy'
-                                />
-                            </div>
-                        </div>
+        <div className='container mx-auto'>
+            <div className="flip-card">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <Image
+                            src="/portafolio/branding-1/item-1.webp"
+                            alt="Front Avatar"
+                            width={300}
+                            height={300}
+                        />
+                    </div>
+                    <div className="flip-card-back">
+                        <Image
+                            src="/portafolio/branding-1/item-2.webp"
+                            alt="Back Avatar"
+                            width={300}
+                            height={300}
+                        />
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     );
 }
 
