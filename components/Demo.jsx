4f9a9d4c7483d4b8d57a8e6f7b098138ffea7ab7
AngelLuna17-code <luna17ahle@gmail.com>
2024-04-18 13:50:09 -0700

    trabajando en animacion de flip image

---
+++ components/Demo.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Demo() {
+    return (
+        <div className="flip-box">
+            <div className="flip-box-inner">
+                <div className="flip-box-front">
+                    <img
+                        src="img_paris.jpg"
+                        alt="Paris"
+                    />
+                </div>
+                <div className="flip-box-back">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Demo;
--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ import Mapa from "../../components/Mapa";
 import Contacto from "../../components/Contacto";
 import PortafolioWeb from "../../components/PortafolioWeb";
 import Portafolio2 from "../../components/Portafolio2"
+import Demo from "../../components/demo";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <About />
         <ServiceCards />
         <Portafolio2 />
+        <Demo />
         <PortafolioWeb />
         <LogoSlider />
         <Mapa />
