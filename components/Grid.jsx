d36d3ff833badd1d7ee689f00c75ea0eb3a17533
AngelLuna17-code <luna17ahle@gmail.com>
2024-04-24 13:55:05 -0700

    estilando grid

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -157,13 +157,13 @@ const Grid = () => {
         <section className='py-[50px] px-[15px]'>
             <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-5'>
                 <div className='order-2 lg:order-1'>
-                    <div className='grid grid-cols-2 lg:grid-cols-4 gap-3 h-[400px] md:h-[600px] lg:h-[600px] overflow-y-auto'>
+                    <div className='grid grid-cols-2 lg:grid-cols-4 gap-3 h-[400px] md:h-[600px] lg:h-[600px] overflow-y-auto rounded-[15px]'>
                         {columnImages.map((column, colIndex) => (
                             <div key={colIndex}
                             >
                                 {column.map((image, imgIndex) => (
                                     <Image
-                                        className='h-auto- w-[100%] mt-3'
+                                        className='h-auto- w-[100%] mt-3 rounded-[15px]'
                                         width={300}
                                         height={300}
                                         key={imgIndex}
