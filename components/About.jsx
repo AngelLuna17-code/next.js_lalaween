d88fbdf7a57514e2546a1725ad2e5b051031fe99
AngelLuna17-code <luna17ahle@gmail.com>
2024-05-03 12:03:30 -0700

    corregido historia

--- components/About.jsx
+++ components/About.jsx
@@ -28,18 +28,43 @@ const About = () => {
         <section className='py-[50px] px-[15px]'>
             <div id="about" className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-5'>
                 <div className='flex items-center justify-center'>
-                    <div className='text-white text-center'>
+                    <div className='text-white'>
                         <h2 className='text-white text-center text-[25px] sm:text-[32px] md:text-[36px] lg:text-[40px] xl-[44px] 2xl:text-[48px] title-container '>
                             ¿Como nació
-                            <br />
+                            <br className='mt-3' />
                             <span className='text-white text-center text-[25px] sm:text-[32px] md:text-[36px] lg:text-[40px] xl-[44px] 2xl:text-[48px] title-container title'>
                                 Lalaween
                             </span>
                             ?
                         </h2>
-                        <p className='mt-3 mx-auto text-center'>
-                            LalaWeen Creative Team nació en 2020, pero su origen se remonta a 2017 cuando su fundadora comenzó como diseñadora freelancer. Inspirada por su gato Rony, decidió emprender su propia agencia en Guadalajara, incorporándolo en el logotipo como una musa. Ofreciendo servicios de diseño y gestión de negocios digitales, comprometidos con hacer realidad los sueños de nuestros clientes, gracias al legado inspirador de Rony. En resumen, LalaWeen es el creative team que toda marca necesita.
-                        </p>
+                        <div className='mt-4'>
+                            <p>
+                                LalaWeen Creative Team, nación el 31 de Octubre del 2020; sin embargo, fue un proyecto
+                                que comenzó en el año 2017, como un trabajo solitario, pero enriquecedor como diseñadora freelancer.
+                            </p>
+                            <p className='mt-3'>
+                                Debido a que, por falta de oportunidades laborales de su área, en su ciudad natal, Tepic,
+                                Nayarit, busco el aprendizaje y retos más grandes en agencias de publicidad en Guadalajara, llevándola lejos de casa y su apreciado Rony.
+                            </p>
+                            <p className='mt-3'>
+                                Fue así, como nuestra fundadora, se puso cómo meta, emprender su propia agencia y trabajar en compañía de su apreciado compañero felino.
+                                Tristemente, Rony se marchó antes de poder iniciar con esa etapa de emprendimiento, pero
+                                nunca dejo de ser fuente de inspiración, entusiasmando a nuestra directora a hacerlo parte
+                                de nuestro logotipo, “la musa” de nuestra marca.
+                            </p>
+                            <p className='mt-3'>
+                                Un gato en la luna, cuidando e inspirándonos desde el cielo,
+                                rodeado de cometas, es decir, sueños y metas por cumplir; Y
+                                con esto nos referimos, no solo a las propias, sino también a
+                                las tuyas, al invitarnos a ser parte del desarrollo de una idea
+                                desde su diseño, creación y manejo de tu negocio, por
+                                medios digitales, apoyándote con nuestro conocimiento y
+                                compromiso, que Rony nos impulsa a seguir, por qué
+                                LalaWeen es más que un negocio, es el “Creative Team”
+                                que toda marca necesita
+                            </p>
+                        </div>
+
                     </div>
                 </div>
                 <div className='flex items-center justify-center'>
